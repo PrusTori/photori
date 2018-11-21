78b780d6098f084d2a5ec5378c4178ccb118e5b2
dima <skaliy.d@gmail.com>
2018-11-21 18:44:34 +0200

    replaced: all anchorpane with hbox and vbox; snoozed: grid photosets; created: tab for administrative users; created: method to create table; created: method to display record in textfiled and combobox; created: method to update record;

--- src/main/java/com/viktoriia/photori/fxapp/app/ControllerPhotori.java
+++ src/main/java/com/viktoriia/photori/fxapp/app/ControllerPhotori.java
@@ -11,42 +11,51 @@ import java.util.HashSet;
 
 import javax.swing.*;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.Label;
 import javafx.fxml.FXML;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.ComboBoxTableCell;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
 import org.intellij.lang.annotations.Language;
 
-public class ControllerPhotori {
 
-    @FXML
-    private JFXTextField textName, textPhone, textMail, textTime,
-            textLocationAddress, textLocationName, textOverPhotoQuantity;
+public class ControllerPhotori {
 
     @FXML
     private Label labelTypeInfo, labelRoomInfo, labelPrice;
 
     @FXML
-    private JFXComboBox<String> comboType, comboRoom, comboPhotoset;
+    private JFXTextField textName, textPhone, textMail, textTime,
+            textLocationAddress, textLocationName, textOverPhotoQuantity,
+            textAdd1, textAdd2, textAdd3, textAdd4;
 
     @FXML
-    private JFXDatePicker date;
+    private JFXComboBox<String> comboType, comboRoom, comboPhotoset, comboTable;
 
     @FXML
-    private JFXTimePicker time;
+    private JFXDatePicker datePicker;
 
     @FXML
-    private JFXButton buttonOrder;
+    private JFXTimePicker timePicker;
 
     @FXML
-    private AnchorPane anchorScrollPhotosets;
+    private JFXButton buttonOrder, buttonAdd, buttonDelete;
+
+    @FXML
+    private VBox vboxScrollPhotosets;
 
     @FXML
     private GridPane gridPhotos;
+
+    @FXML
+    private TableView<String[]> tableView;
 
     private DatabaseConnector databaseConnector;
 
@@ -63,6 +72,7 @@ public class ControllerPhotori {
 
         comboRoom.setItems(setItemsCombo("select name from rooms order by name"));
         comboType.setItems(setItemsCombo("select title from types order by title"));
+        comboPhotoset.setItems(setItemsCombo("select title from photosets order by title"));
 
         comboRoom.setOnAction(event -> {
 
@@ -108,19 +118,29 @@ public class ControllerPhotori {
             calculatePriceTime();
 
         });
+        comboPhotoset.setOnAction(event -> {
+//            anchorScrollPhotosets.getChildren().removeAll(gridPhotos);
+            if (comboPhotoset.getSelectionModel().isSelected(0)) {
+                comboPhotoset.getSelectionModel().clearSelection();
+            } else {
+                selectPhotoset();
+            }
+        });
+
         textOverPhotoQuantity.setOnKeyReleased(event -> {
             calculatePriceTime();
         });
         textTime.setOnKeyReleased(event -> {
             calculatePrice();
         });
+
         buttonOrder.setOnAction(event -> {
 
             if ((textName.getText().isEmpty()
                     || textPhone.getText().isEmpty()
                     || textMail.getText().isEmpty()
-                    || date.getValue() == null
-                    || time.getValue() == null
+                    || datePicker.getValue() == null
+                    || timePicker.getValue() == null
                     || textTime.getText().isEmpty()
                     || comboType.getSelectionModel().isEmpty())
                     && (comboRoom.getSelectionModel().isEmpty()
@@ -141,7 +161,7 @@ public class ControllerPhotori {
                                 + textMail.getText() + "', '" +
                                 comboType.getSelectionModel().getSelectedItem() + "', " +
                                 "to_timestamp('" +
-                                date.getValue().toString() + " " + time.getValue().toString() +
+                                datePicker.getValue().toString() + " " + timePicker.getValue().toString() +
                                 "', 'YYYY-MM-DD HH24:MI'), " +
                                 textTime.getText() + ", " +
                                 (comboRoom.getSelectionModel().getSelectedIndex() == 0
@@ -168,15 +188,70 @@ public class ControllerPhotori {
 
         });
 
-        comboPhotoset.setItems(setItemsCombo("select title from photosets order by title"));
-        comboPhotoset.setOnAction(event -> {
-            anchorScrollPhotosets.getChildren().removeAll(gridPhotos);
-            if (comboPhotoset.getSelectionModel().isSelected(0)) {
-                comboPhotoset.getSelectionModel().clearSelection();
-            } else {
-                selectPhotoset();
+        comboTable.setItems(
+                FXCollections.observableArrayList(
+                        "- выбор -",
+                        "Клиенты",
+                        "Локации",
+                        "Комнаты",
+                        "Типы фотосетов",
+                        "Фотосеты",
+                        "Фотографии",
+                        "Заказы"
+                )
+        );
+        comboTable.setOnAction(event -> {
+
+            switch (comboTable.getSelectionModel().getSelectedIndex()) {
+
+                case 0:
+
+                    break;
+
+                case 1:
+
+                    setTable(
+                            "select * from clients order by full_name",
+                            new String[]{"id_client", "full_name", "phone", "e-mail"},
+                            null, null
+                    );
+                    showAndSetValue(
+                            new JFXTextField[]{textAdd1, textAdd2, textAdd3},
+                            new int[]{1, 2, 3},
+                            new String[]{"ФИО", "Телефон", "Email"},
+                            null, null, null
+                    );
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    break;
+
+                case 6:
+
+                    break;
+
+                case 7:
+
+                    break;
+
             }
+
         });
+
 
     }
 
@@ -248,7 +323,7 @@ public class ControllerPhotori {
 
     private void selectPhotoset() {
 
-        photos = databaseConnector.getSql(
+        /*photos = databaseConnector.getSql(
                 "select * from photos where id_photoset = " +
                         "(select id_photoset from photosets where title = '" +
                         comboPhotoset.getSelectionModel().getSelectedItem() + "') " +
@@ -256,17 +331,21 @@ public class ControllerPhotori {
         );
 
         gridPhotos = new GridPane();
+        gridPhotos.setAlignment(Pos.CENTER);
+        gridPhotos.setPrefWidth(anchorScrollPhotosets.getPrefWidth());
 
         for (int i = 1, j = 0, k = 0; i < photos.length; i++) {
 
             ImageView imageViewPhoto = new ImageView(photos[i][2]);
-            imageViewPhoto.setFitHeight(150);
-            imageViewPhoto.setFitWidth(230);
-            imageViewPhoto.setOpacity(0.75);
-            imageViewPhoto.setOnMouseEntered(event -> imageViewPhoto.setOpacity(1));
-            imageViewPhoto.setOnMouseExited(event -> imageViewPhoto.setOpacity(0.75));
 
-            if (j == 3) {
+//            imageViewPhoto.setFitHeight(250);
+//            imageViewPhoto.setFitWidth(230);
+
+            imageViewPhoto.setOpacity(0.8);
+            imageViewPhoto.setOnMouseEntered(event -> imageViewPhoto.setOpacity(1));
+            imageViewPhoto.setOnMouseExited(event -> imageViewPhoto.setOpacity(0.8));
+
+            if (j == 1) {
                 j = 0;
                 k++;
             }
@@ -276,8 +355,136 @@ public class ControllerPhotori {
 
         anchorScrollPhotosets.getChildren().addAll(gridPhotos);
         anchorScrollPhotosets.getChildren().get(0).setLayoutX(5.5);
-        anchorScrollPhotosets.setPrefHeight(gridPhotos.getPrefHeight());
+        anchorScrollPhotosets.setPrefHeight(gridPhotos.getPrefHeight());*/
 
     }
+
+    private void setOrientation() {
+
+    }
+
+
+    private void setTable(@Language("SQL") String sql, String[] colName,
+                          ObservableList<String>[] comboItems, int[] indexColumnCombo) {
+
+        tableView.getColumns().clear();
+        tableView.getItems().clear();
+
+        String[][] records = databaseConnector.getSql(sql);
+
+        for (int i = 0, j = 0; i < records[0].length; i++) {
+            TableColumn<String[], String> tableColumn = new TableColumn<>(colName[i]);
+            final int col = i;
+            tableColumn.setCellValueFactory(
+                    (TableColumn.CellDataFeatures<String[], String> param) -> new SimpleStringProperty(param.getValue()[col]));
+
+            boolean combo = false;
+
+            if (comboItems == null && indexColumnCombo == null) {
+                tableColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+                tableColumn.setEditable(true);
+            } else {
+                for (int indexColumn : indexColumnCombo)
+                    if (i == indexColumn) {
+                        combo = true;
+                        break;
+                    }
+            }
+            if (combo) {
+                tableColumn.setCellFactory(ComboBoxTableCell.forTableColumn(comboItems[j++]));
+            } else {
+                tableColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+            }
+            tableView.getColumns().add(tableColumn);
+        }
+
+        ObservableList<String[]> items = FXCollections.observableArrayList(records);
+
+        tableView.setItems(items);
+        tableView.getItems().remove(0);
+        tableView.getColumns().get(0).setVisible(false);
+        tableView.setEditable(true);
+
+    }
+
+
+    private void showAndSetValue(JFXTextField[] textFields, int[] indexFields, String[] promptFields,
+                                 JFXComboBox[] comboBoxes, int[] indexCombos, String[] promptCombos) {
+
+        boolean isTextField = textFields != null && indexFields != null,
+                isCombo = comboBoxes != null && indexCombos != null;
+
+        if (isTextField) {
+            int i = 0;
+            for (JFXTextField textField : textFields) {
+                textField.setDisable(false);
+                textField.setVisible(true);
+                textField.setPromptText(promptFields[i++]);
+            }
+        }
+
+        if (isCombo) {
+            int i = 0;
+            for (JFXComboBox comboBox : comboBoxes) {
+                comboBox.setDisable(false);
+                comboBox.setVisible(true);
+                comboBox.setPromptText(promptCombos[i++]);
+            }
+        }
+
+        tableView.setOnMouseClicked(action -> {
+
+            if (!tableView.getSelectionModel().isEmpty()) {
+                buttonAdd.setDisable(false);
+                buttonDelete.setDisable(false);
+
+                if (isTextField) {
+
+                    for (int t = 0; t < textFields.length; t++) {
+                        for (int j = 0; j < tableView.getColumns().size(); j++) {
+                            if (j == indexFields[t]) {
+                                textFields[t].setText(tableView.getSelectionModel().getSelectedItem()[j]);
+                            }
+                        }
+                    }
+
+                } else {
+                    buttonAdd.setDisable(true);
+                }
+
+                if (isCombo) {
+
+                    for (int c = 0; c < comboBoxes.length; c++) {
+                        for (int j = 0; j < tableView.getColumns().size(); j++) {
+                            if (j == indexCombos[c]) {
+                                comboBoxes[c].setValue(tableView.getSelectionModel().getSelectedItem()[j]);
+                            }
+                        }
+                    }
+
+                }
+
+            }
+
+        });
+    }
+
+
+    private void editCellTable(String tableName) {
+
+        for (int i = 1; i < tableView.getColumns().size(); i++) {
+            int finalI = i;
+            tableView.getColumns().get(i).setOnEditCommit(actionEdit -> {
+                tableView.getFocusModel().getFocusedItem()[finalI] = String.valueOf(actionEdit.getNewValue());
+
+                /*boolean update = databaseConnector.sql("update " + tableName
+                        + "," + finalI + "," + tableView.getFocusModel().getFocusedItem()[finalI]
+                        + "," + tableView.getFocusModel().getFocusedItem()[0]);
+*/
+            });
+
+        }
+    }
+
 
 }
